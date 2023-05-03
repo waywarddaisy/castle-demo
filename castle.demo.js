a69bc60e50220540a33118ab664ad64df44d5bf5
waywarddaisy <jenna@waywarddaisy.com>
2023-05-03 08:46:29 -0700

    entered all data, ready for css

--- castle.demo.js
+++ castle.demo.js
@@ -1,63 +1,152 @@
 
-const createMenuButtons = (item)=>{
+const createMenuButtons = (item) => {
     let menuContainer = document.getElementById('menu-container');
-    let menuLink =document.createElement('a');
+    let menuLink = document.createElement('a');
     menuLink.className = `page-menu-button ${item.ClassList}`;
     menuLink.href = item.Endpoint;
-    menuLink.innerHTML=item.DisplayValue;
+    menuLink.innerHTML = item.DisplayValue;
     menuContainer.appendChild(menuLink);
 };
 
-const createMenu = (data)=> {
-    let menuArray=data.PageMenu;
+const createMenu = (data) => {
+    let menuArray = data.PageMenu;
     console.log(menuArray);
     menuArray.forEach(item => {
         createMenuButtons(item)
-    } );
+    });
 };
 
-const createRightPanel =()=> {
-    let rightPanel= document.getElementById('right-panel');
-    let rightRow=document.createElement('div');
+const createRightPanel = () => {
+    let rightPanel = document.getElementById('right-panel');
+    //Top row div
+    let rightRow = document.createElement('div');
     rightRow.id = "right-panel-row";
     rightPanel.appendChild(rightRow);
-    let rightImgDiv=document.createElement('div');
+
+    //Pricing Div
+    let rightPricing = document.createElement('div');
+    rightPricing.className = "right-pricing";
+    rightPanel.appendChild(rightPricing);
+
+    //NSO/RPL Pricing
+    let nsoRPL = document.createElement('div');
+    nsoRPL.className = "pricing";
+    nsoRPL.id = "nso-rpl";
+    rightPricing.appendChild(nsoRPL);
+
+    //sjp pricing
+    let sjp = document.createElement('div');
+    sjp.className = "pricing";
+    sjp.id = "spj";
+    rightPricing.appendChild(sjp);
+
+    //diagram div 
+    let rightImgDiv = document.createElement('div');
     rightImgDiv.id = 'right-image-div';
     rightPanel.appendChild(rightImgDiv);
-    let rightCartonsDiv= document.createElement('div');
-    rightCartonsDiv.id= "cartons";
+
+    //cartons
+    let rightCartonsDiv = document.createElement('div');
+    rightCartonsDiv.id = "cartons";
     rightPanel.appendChild(rightCartonsDiv);
+
+    //parent products
     let rightParentProducts = document.createElement('div');
-    rightParentProducts.className="Parent Products";
+    rightParentProducts.id = "Parent Products";
     rightPanel.appendChild(rightParentProducts);
 };
 
-const populateRightPanel =(data)=> {
+const populateRightPanel = (data) => {
     createRightPanel();
     //row
     let rightPanelData = data.InventoryTags;
 
     rightPanelData.forEach(item => {
-        let button =document.createElement('button');
-        button.type= "button";
+        let button = document.createElement('button');
+        button.type = "button";
         button.innerHTML = item.DisplayValue;
         button.className = "right-panel-row";
-        let rightRow= document.getElementById('right-panel-row');
+        let rightRow = document.getElementById('right-panel-row');
         rightRow.appendChild(button);
 
     });
-    //image
+
+    //NSO/RPL Pricing
+
+    let NSO = document.getElementById('nso-rpl');
+    let nsoHeader = document.createElement('h4');
+    nsoHeader.innerHTML = "NSO/RPL Pricing:";
+    NSO.appendChild(nsoHeader);
+    data.NsoPricingTags.forEach(item => {
+        let button = document.createElement('button');
+        button.innerHTML = item.DisplayValue;
+        button.type = "button";
+        button.className = "pricing-button";
+        NSO.appendChild(button)
+    });
+
+    //SPJ Pricing
+    let SPJ = document.getElementById('spj');
+    let spjHeader = document.createElement('h4');
+    spjHeader.innerHTML = "SPJ Pricing:";
+    SPJ.appendChild(spjHeader);
+    data.SpjPricingTags.forEach(item => {
+        let button = document.createElement('button');
+        button.innerHTML = item.DisplayValue;
+        button.type = "button";
+        button.className = "pricing-button";
+        SPJ.appendChild(button)
+    });
+
+    //diagram
     let rightImgDiv = document.getElementById('right-image-div');
-    rightImgDiv.style.backgroundColor="blue";
     let rightImg = document.createElement('img');
-    let imgValue = data.PageTabs.find(item => item.DisplayValue==='Diagrams');
-    rightImg.href =imgValue.Endpoint;
+    let imgValue = data.PageTabs.find(item => item.DisplayValue === 'Diagrams');
+    rightImg.href = imgValue.Endpoint;
     rightImgDiv.appendChild(rightImg);
-    //other info
+
+    //Cartons
+    let cartonsDiv = document.getElementById('cartons');
+    let cartonsHeader = document.createElement('h4');
+    cartonsHeader.innerHTML = "Cartons";
+    cartonsDiv.appendChild(cartonsHeader);
+    data.Cartons.forEach(item=> {
+        let section = document.createElement('div');
+        section.className = "right-cartons-parent-section";
+        let key = document.createElement('p');
+        key.className = "key";
+        key.innerHTML = item.Description;
+        section.appendChild(key);
+        let value = document.createElement('p');
+        value.className = "value";
+        value.innerHTML = item.Weight;
+        section.appendChild(value); 
+        cartonsDiv.appendChild(section);
+    });
+
+    //Parent Products
+
+    let parentProductsDiv = document.getElementById('Parent Products');
+    let pProductsHeader = document.createElement('h4');
+    pProductsHeader.innerHTML = "Parent Products";
+    parentProductsDiv.appendChild(pProductsHeader);
+    data.ParentProducts.forEach(item=> {
+        let section = document.createElement('div');
+        section.className = "right-cartons-parent-section";
+        let key = document.createElement('p');
+        key.className = "key";
+        key.innerHTML = item.Description;
+        section.appendChild(key);
+        let value = document.createElement('p');
+        value.className = "value";
+        value.innerHTML = item.Weight;
+        section.appendChild(value); 
+        parentProductsDiv.appendChild(section);
+    });
 
 };
 
-const createBottomTabs =(data)=>{
+const createBottomTabs = (data) => {
     let bottomTabs = document.getElementById('bottom-tabs');
     data.PageTabs.forEach(item => {
         let newTab = document.createElement('a');
@@ -69,61 +158,59 @@ const createBottomTabs =(data)=>{
 
 };
 
-const createProductHeader =(data)=>{
-let productName= document.getElementById("product-title");
-productName.innerHTML=data.Title;
-let productSubtitle = document.getElementById("subtitle");
-productSubtitle.innerHTML=data.SubTitle;
-let productNav =document.getElementById('product-nav');
-let productNavUl =document.createElement('ul');
-productNav.appendChild(productNavUl);
-let pageLinks =data.PageLinks;
-pageLinks.forEach(link =>{
-    let productNavLi = document.createElement('li');
-    let prodNavLink =document.createElement('a');
-    prodNavLink.href=link.Endpoint;
-    productNavLi.appendChild(prodNavLink);
-    prodNavLink.innerHTML =link.DisplayValue;
-    productNavUl.appendChild(productNavLi);
-});
+const createProductHeader = (data) => {
+    let productName = document.getElementById("product-title");
+    productName.innerHTML = data.Title;
+    let productSubtitle = document.getElementById("subtitle");
+    productSubtitle.innerHTML = data.SubTitle;
+    let productNav = document.getElementById('product-nav');
+    let productNavUl = document.createElement('ul');
+    productNav.appendChild(productNavUl);
+    let pageLinks = data.PageLinks;
+    pageLinks.forEach(link => {
+        let productNavLi = document.createElement('li');
+        let prodNavLink = document.createElement('a');
+        prodNavLink.href = link.Endpoint;
+        productNavLi.appendChild(prodNavLink);
+        prodNavLink.innerHTML = link.DisplayValue;
+        productNavUl.appendChild(productNavLi);
+    });
 
 };
 
-const createProductInfo =(data)=>{
+const createProductInfo = (data) => {
     let prodInfoDiv = document.getElementById('product-info');
     let prodInfoArray = data.Record;
     prodInfoArray.forEach(record => {
         let infoContainer = document.createElement('div');
-        infoContainer.className ="info-group";
+        infoContainer.className = "info-group";
         let key = document.createElement('div');
-        key.className='info-key';
-        key.innerHTML= record.Label;
+        key.className = 'info-key';
+        key.innerHTML = record.Label;
         infoContainer.appendChild(key);
-        let value =document.createElement('div');
-        value.className='info-value';
-        value.innerHTML=record.DisplayValue;
+        let value = document.createElement('div');
+        value.className = 'info-value';
+        value.innerHTML = record.DisplayValue;
         infoContainer.appendChild(value);
         prodInfoDiv.appendChild(infoContainer);
     })
 };
 
 
-const populatePage =()=>{
-    const getJSON = async ()=> {
+const populatePage = () => {
+    const getJSON = async () => {
         try {
             const response = await fetch('./castle.demo.json');
             const data = await response.json();
-            console.log(data);
-            console.log(data.Title);
             createMenu(data);
             createProductHeader(data);
             createProductInfo(data);
             populateRightPanel(data);
             createBottomTabs(data);
-        } catch (err) {console.log('error', err)};   
-        
+        } catch (err) { console.log('error', err) };
+
     };
-getJSON();
+    getJSON();
 };
 
 populatePage();
